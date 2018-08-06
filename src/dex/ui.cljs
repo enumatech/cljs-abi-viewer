c8e7ae28c098137c897323060fb63e1421d26911
Tamas Herman <tamas.herman@enuma.io>
2018-08-06 10:29:14 +0800

    Generalizes app state-history viewer

--- src/dex/ui.cljs
+++ src/dex/ui.cljs
@@ -1,4 +1,4 @@
-(ns ^:figwheel-hooks dex.viewer
+(ns ^:figwheel-hooks dex.ui
   (:require
     [cljs.dom
      :refer [Elem render log mount $ text elem frag fragment
@@ -27,11 +27,6 @@
   (render [{:keys [party buy sell]}]
     (span {} party (i "has ") buy " & " (i "wants ") sell "")))
 
-; Application time
-(defonce T (atom 0))
-
-(def #_once state (atom nil))
-
 (defn order-book> [order-book]
   (v-array order-book))
 
@@ -44,7 +39,7 @@
       (h3 "Order book")
       (order-book> order-book))))
 
-(defn app [state]
+(defn ui [state]
   (fragment
     (h1 "DEX (time: " (:time state) ")")
     ;(h-map (get state Alice))
@@ -58,46 +53,3 @@
              (td (party> (get state Alice)))
              (td (get state :whisper))
              (td (party> (get state Bob)))))))
-
-(defn shift? [^js/KeyboardEvent ev]
-  (and (.-shiftKey ev)
-       (not (.-ctrlKey ev))
-       (not (.-altKey ev))
-       (not (.-metaKey ev))))
-
-(defn shift-left? [^js/KeyboardEvent ev]
-  (and (shift? ev) (= (.-key ev) "ArrowLeft")))
-
-(defn shift-right? [^js/KeyboardEvent ev]
-  (and (shift? ev) (= (.-key ev) "ArrowRight")))
-
-(defn dispatch-keypress [^js/KeyboardEvent ev]
-  ;(log "Key:" ev)
-  (cond
-    (shift-left? ev) (swap! T dec)
-    (shift-right? ev) (swap! T inc)))
-
-(defonce keypresses
-         (atom (js/document.addEventListener "keydown" dispatch-keypress)))
-
-(defn show-app [state]
-  ;(log :state state)
-  (time (mount ($ "#app") [(app state)])))
-
-(defn on-state-change [_key _ref old new]
-  (when-not (= old new)
-    (show-app new)))
-
-(add-watch state :app-state on-state-change)
-
-(defn app-state-at [t]
-  (assoc (get demo/steps t) :time t))
-
-(defn on-time-change [_key _ref _old-time new-time]
-  (reset! state (app-state-at new-time)))
-
-(add-watch T :app-time on-time-change)
-
-; Force app-state recalculation
-(swap! T identity)
-(show-app @state)
---
+++ src/history/viewer.cljs
@@ -0,0 +1,64 @@
+(ns ^:figwheel-hooks history.viewer
+  (:require
+    [cljs.dom
+     :refer [Elem render log mount $ text elem frag fragment
+             div span i img br hr h1 h2 h3 h4 hr ul li table tr th td
+             v-array h-array v-map h-map x xi]]
+    [dex.demo]
+    [dex.ui]))
+
+(def steps dex.demo/steps)
+
+; Application time
+(defonce T (atom 0))
+
+(def #_once state (atom nil))
+
+(defn viewer [state]
+  (fragment
+    (h1 "Application history viewer (time: " (:time state) ")")
+    (h2 "State")
+    (dex.ui/ui state)))
+
+(defn shift? [^js/KeyboardEvent ev]
+  (and (.-shiftKey ev)
+       (not (.-ctrlKey ev))
+       (not (.-altKey ev))
+       (not (.-metaKey ev))))
+
+(defn shift-left? [^js/KeyboardEvent ev]
+  (and (shift? ev) (= (.-key ev) "ArrowLeft")))
+
+(defn shift-right? [^js/KeyboardEvent ev]
+  (and (shift? ev) (= (.-key ev) "ArrowRight")))
+
+(defn dispatch-keypress [^js/KeyboardEvent ev]
+  ;(log "Key:" ev)
+  (cond
+    (shift-left? ev) (swap! T dec)
+    (shift-right? ev) (swap! T inc)))
+
+(defonce keypresses
+         (atom (js/document.addEventListener "keydown" dispatch-keypress)))
+
+(defn show-app [state]
+  ;(log :state state)
+  (time (mount ($ "#app") [(viewer state)])))
+
+(defn on-state-change [_key _ref old new]
+  (when-not (= old new)
+    (show-app new)))
+
+(add-watch state :app-state on-state-change)
+
+(defn app-state-at [t]
+  (assoc (get steps t) :time t))
+
+(defn on-time-change [_key _ref _old-time new-time]
+  (reset! state (app-state-at new-time)))
+
+(add-watch T :app-time on-time-change)
+
+; Force app-state recalculation
+(swap! T identity)
+(show-app @state)
